48da3adf459abe93669354e77ec944d2d0ef2894
MattSelwood <ms16439@bristol.ac.uk>
2022-05-13 14:40:59 +0100

    Update recipes to latest versions
    Default:
    - Source options
    
    DP:
    - Remove Ha core
    - Unknown Avoid for [OIII]4959
    - FWHM constraints / initial guesses
    - Patches

--- Type2AGN_DP.jl
+++ Type2AGN_DP.jl
@@ -5,6 +5,10 @@ abstract type Type2AGN_DP <: Type2AGN end
 function QSFit.default_options(::Type{T}) where T <: Type2AGN_DP
     out = default_options(supertype(T))
     out[:n_unk] = 4
+    out[:unk_avoid] = [4863 .+ [-1,1] .* 50, 
+                       6565 .+ [-1,1] .* 150,
+                       5008 .+ [-1,1] .* 25,
+                       4959 .+ [-1,1] .* 25]
     return out
 end
 
@@ -18,7 +22,7 @@ function QSFit.known_spectral_lines(source::QSO{T}) where T <: Type2AGN_DP
     push!(list, NarrowLine(:OIII_5007, cname=:OIII_5007_2))
     push!(list, NarrowLine(:OIII_5007, cname=:OIII_5007_core))
     push!(list, NarrowLine(:Ha, cname=:Ha_2))
-    push!(list, NarrowLine(:Ha, cname=:Ha_core))
+    #push!(list, NarrowLine(:Ha, cname=:Ha_core))
     push!(list, NarrowLine(:NII_6583, cname=:NII_6583_2))
     return list
 end
@@ -27,12 +31,11 @@ end
 function QSFit.LineComponent(source::QSO{T}, line::NarrowLine, multicomp::Bool) where T <: Type2AGN_DP
     lc = LineComponent(parent_recipe(source), line, multicomp)
     #lc.comp.fwhm.low = 100
-    #lc.comp.fwhm.high = 500
-    lc.comp.fwhm.val = 100
+    lc.comp.fwhm.high = 500
+    #lc.comp.fwhm.val = 100
     if line.cname in [:OII_3727, :Hb_2, :OIII_4959_2, :OIII_5007_2, :Ha_2, :NII_6583_2]
-        lc.comp.voff.val  = 100
         lc.comp.voff.low  = 100
-        lc.comp.voff.high = 1000
+        lc.comp.voff.high = 500
     end
     if line.cname in [:OIII_4959_core, :OIII_5007_core, :Ha_core]
         lc.comp.fwhm.high = 1000
@@ -54,14 +57,14 @@ function QSFit.add_patch_functs!(source::QSO{T}, pspec::PreparedSpectrum, model:
     @try_patch! begin # Hb
         model[:Hb_2].voff += model[:Hb].voff
         model[:Hb].voff = model[:Ha].voff
-        #model[:Hb].fwhm = model[:OIII_5007].fwhm
+       # model[:Hb].fwhm = model[:OIII_5007].fwhm
         model[:Hb_2].voff = model[:Ha_2].voff
         #model[:Hb_2].fwhm = model[:OIII_5007_2].fwhm
     end
     
     @try_patch! begin # OIII_4959
         model[:OIII_4959_2].voff = model[:OIII_5007_2].voff
-        #model[:OIII_4959_2].fwhm = model[:OIII_5007_2].fwhm
+        model[:OIII_4959_2].fwhm = model[:OIII_5007_2].fwhm
         model[:OIII_4959_core].fwhm += model[:OIII_4959].fwhm
         model[:OIII_4959_core].voff = model[:OIII_5007_core].voff
         model[:OIII_4959_core].fwhm = model[:OIII_5007_core].fwhm
@@ -71,16 +74,16 @@ function QSFit.add_patch_functs!(source::QSO{T}, pspec::PreparedSpectrum, model:
     @try_patch! begin # OIII_5007
         model[:OIII_5007_2].voff += model[:OIII_5007].voff
         model[:OIII_5007_core].fwhm += model[:OIII_5007].fwhm
-        #model[:OIII_5007_bw].voff += model[:OIII_5007].voff
+        model[:OIII_5007_bw].voff += model[:OIII_5007].voff
     end
 
     @try_patch! begin # Ha
         model[:Ha_2].voff += model[:Ha].voff
         model[:Ha_core].fwhm += model[:Ha].fwhm
-        #model[:Ha].voff = model[:OIII_5007].voff
-        #model[:Ha].fwhm = model[:OIII_5007].fwhm
-        #model[:Ha_2].voff = model[:OIII_5007_2].voff
-        #model[:Ha_2].fwhm = model[:OIII_5007_2].fwhm
+        model[:Ha].voff = model[:Hb].voff
+        model[:Ha].fwhm = model[:Hb].fwhm
+        model[:Ha_2].voff = model[:Hb_2].voff
+        model[:Ha_2].fwhm = model[:Hb_2].fwhm
     end
 
     @try_patch! begin
@@ -90,7 +93,4 @@ function QSFit.add_patch_functs!(source::QSO{T}, pspec::PreparedSpectrum, model:
         model[:NII_6583].voff = model[:OIII_5007].voff
         model[:NII_6583].fwhm = model[:OIII_5007].fwhm
     end
-
-
-    
 end
