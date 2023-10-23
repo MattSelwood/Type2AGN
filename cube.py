acfe14889cdc87765cdf240c47e609f3c6f456e2
MattSelwood <ms16439@bristol.ac.uk>
2023-10-23 14:01:44 +0100

    Create cube.py

---
+++ cube.py
@@ -0,0 +1,60 @@
+import numpy as np
+from astropy.io import fits
+
+#  Load cube with astropy
+cube = fits.open("data/manga-11955-12703-LOGCUBE.fits.gz")
+
+print(cube.info())
+
+FLUX = cube[1].data
+IVAR = cube[2].data
+MASK = cube[3].data
+WAVELENGTH = cube[6].data
+
+print(FLUX.shape)
+
+x = 16
+y = 16
+
+spaxel_flux = FLUX[:, x, y]
+spaxel_ivar = IVAR[:, x, y]
+spaxel_mask = MASK[:, x, y]
+spaxel_wavelength = WAVELENGTH
+
+spaxel_sigma = np.sqrt((1 / spaxel_ivar))
+
+import matplotlib.pyplot as plt
+plt.style.use('/Users/ms16439/OneDrive - University of Bristol/PhD/mplstyles/mplrc_sotiria.mplstyle')
+
+plt.figure(figsize=(7, 5))
+plt.vlines(spaxel_wavelength[spaxel_mask > 0],
+           ymin=-0.2,
+           ymax=0.5,
+           color='tab:orange', 
+           alpha=0.2,
+           label='Mask')
+plt.errorbar(
+    x=spaxel_wavelength, 
+    y=spaxel_flux, 
+    yerr=spaxel_sigma,
+    fmt='o',
+    ecolor='silver',
+    color='k',
+    ms=0,
+    label='Uncertainty'
+    )
+plt.plot(spaxel_wavelength, 
+         spaxel_flux, 
+         color='k', 
+         marker=',', 
+         label='Data',
+         lw=1
+         )
+
+plt.ylim((-0.2, 0.5))
+plt.xlim((min(spaxel_wavelength), max(spaxel_wavelength)))
+plt.xlabel(r'Wavelength ($\AA$)')
+plt.ylabel(r'Flux (10$^{-17}\,$erg$\,$s$^{-1}\,$cm$^{-2}$$\,\AA^{-1}$)')
+plt.legend()
+plt.show()
+plt.close()
