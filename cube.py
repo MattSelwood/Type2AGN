f21d0d202869c8a7c560efc97e0d2afa2b114b44
MattSelwood <ms16439@bristol.ac.uk>
2023-11-14 09:28:07 +0000

    cube update

--- cube.py
+++ cube.py
@@ -26,7 +26,7 @@ spaxel_sigma = np.sqrt((1 / spaxel_ivar))
 import matplotlib.pyplot as plt
 plt.style.use('/Users/ms16439/OneDrive - University of Bristol/PhD/mplstyles/mplrc_sotiria.mplstyle')
 
-plt.figure(figsize=(7, 5))
+plt.figure(figsize=(10, 5))
 plt.vlines(spaxel_wavelength[spaxel_mask > 0],
            ymin=-0.2,
            ymax=0.5,
@@ -51,10 +51,18 @@ plt.plot(spaxel_wavelength,
          lw=1
          )
 
-plt.ylim((-0.2, 0.5))
-plt.xlim((min(spaxel_wavelength), max(spaxel_wavelength)))
+plt.axvline(6564, color='r', label=r'H$\alpha$, rest')
+plt.axvline(6757, color='r', linestyle='--', label=r'H$\alpha$, observed')
+plt.axvline(4800, color='g')
+
+plt.ylim((-0., 0.5))
+plt.xlim((6500, 7000))
+#plt.xlim((min(spaxel_wavelength), max(spaxel_wavelength)))
 plt.xlabel(r'Wavelength ($\AA$)')
 plt.ylabel(r'Flux (10$^{-17}\,$erg$\,$s$^{-1}\,$cm$^{-2}$$\,\AA^{-1}$)')
 plt.legend()
 plt.show()
 plt.close()
+
+
+redshift = abs(6564-6757) / 6564
