5ae93d375e491a427a869ba55bd0b2c775e70e5b
Giorgio Calderone <giorgio.calderone@gmail.com>
2024-04-07 23:57:05 +0200

    Updated to QSFit release

--- src/Type2AGN_HSN.jl
+++ src/Type2AGN_HSN.jl
@@ -1,13 +1,29 @@
-include("Type2AGN.jl")
+abstract type Type2_AGN_HSN <: Type2_AGN end
+export Type2_AGN_HSN
 
-abstract type Type2AGN_HSN <: Type2AGN end
+function init_recipe!(recipe::Recipe{<: Type2_AGN_HSN})
+    @invoke init_recipe!(recipe::Recipe{<: Type2_AGN})
+    recipe.n_nuisance = 6
 
-function QSFit.Options(::Type{T}) where T <: Type2AGN_HSN
-    out = Options(supertype(T))
-    out[:n_unk] = 6
-    out[:lines][:Hb_2]        = StdEmLine(:Hb       , :narrow)
-    out[:lines][:OIII_4959_2] = StdEmLine(:OIII_4959, :narrow)
-    out[:lines][:OIII_5007_2] = StdEmLine(:OIII_5007, :narrow)
-    out[:lines][:Ha_2]        = StdEmLine(:Ha       , :narrow)
-    return out
+    recipe.lines = [
+        LineDescriptor(:Lya       , NarrowLine),
+        LineDescriptor(:NV_1241   , ForbiddenLine),
+        LineDescriptor(:CIV_1549  , NarrowLine),
+        LineDescriptor(:CIII_1909 , NarrowLine),
+        LineDescriptor(:MgII_2798 , NarrowLine),
+        LineDescriptor(:NeV_3426  , ForbiddenLine),
+        LineDescriptor(:OII_3727  , ForbiddenLine),
+        LineDescriptor(:NeIII_3869, ForbiddenLine),
+        LineDescriptor(:Hg        , NarrowLine),
+        LineDescriptor(:Hb        , NarrowLine, SecondComponent),
+        LineDescriptor(:OIII_4959 , ForbiddenLine, QSFit.QSORecipes.BlueWing, SecondComponent),
+        LineDescriptor(:OIII_5007 , ForbiddenLine, QSFit.QSORecipes.BlueWing, SecondComponent),
+        LineDescriptor(:OI_6300   , ForbiddenLine),
+        LineDescriptor(:OI_6364   , ForbiddenLine),
+        LineDescriptor(:NII_6549  , ForbiddenLine),
+        LineDescriptor(:Ha        , NarrowLine, SecondComponent),
+        LineDescriptor(:NII_6583  , ForbiddenLine),
+        LineDescriptor(:SII_6716  , ForbiddenLine),
+        LineDescriptor(:SII_6731  , ForbiddenLine)
+    ]
 end
